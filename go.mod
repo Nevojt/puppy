f257aa26bb090bd7ca63f4b70d1ae283ee323699
Nevojt <nevojt@gmail.com>
2025-01-02 20:57:16 +0100

    quick

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Nevojt/puppy
 
 go 1.23
+
+require github.com/Nevojt/dog v0.0.0-20250102195213-c094604e79bc // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,20 @@
 package puppy
 
+import "github.com/Nevojt/dog"
+
 func Burk() string {
 	return "Woof!"
 }
 
 func Burks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrowUp(BigBark())
+
+}
+
+func BigBarks() string {
+	return dog.WhenGrowUp(BigBarks())
 }
